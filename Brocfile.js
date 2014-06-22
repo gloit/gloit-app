11791566ef0375946440fa9eb48cf65c41e3dbb6
Tower He <towerhe@gmail.com>
2014-06-23 01:29:58 +0800

    Upgrade to ember-cli 0.0.36.

--- Brocfile.js
+++ Brocfile.js
@@ -4,49 +4,20 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
 
-var app = new EmberApp({
-  name: require('./package.json').name,
+var app = new EmberApp();
 
-  // for some large projects, you may want to uncomment this (for now)
-  es3Safe: true,
-
-  minifyCSS: {
-    enabled: true,
-    options: {}
-  },
-
-  getEnvJSON: require('./config/environment')
-});
-
-// Use `app.import` to add additional libraries to the generated output files.
+// Use `app.import` to add additional libraries to the generated
+// output files.
 //
-// If you want to use different assets in different
+// If you need to use different assets in different
 // environments, specify an object as the first parameter. That
 // object's keys should be the environment name and the values
-// should be the asset to be use in that environment.
-
-// If the library that you are including contains AMD or ES6 modules that
-// you would like to import into your application please specify an
-// object with the list of modules as keys along with the exports of each
-// module as its value.
-app.import({
-  development: 'vendor/ember-data/ember-data.js',
-  production: 'vendor/ember-data/ember-data.prod.js',
-}, {
-  'ember-data': [
-    'default'
-  ]
-});
-
-app.import('vendor/ic-ajax/dist/named-amd/main.js', {
-  'ic-ajax': [
-    'default',
-    'defineFixture',
-    'lookupFixture',
-    'raw',
-    'request',
-  ]
-});
+// should be the asset to use in that environment.
+//
+// If the library that you are including contains AMD or ES6
+// modules that you would like to import into your application
+// please specify an object with the list of modules as keys
+// along with the exports of each module as its value.
 
 app.import('vendor/select2/select2.css');
 app.import('vendor/select2/select2-bootstrap.css');
