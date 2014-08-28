7a214a1e37aefe7623cbed1beb7bdd47c8c74ce1
Tower He <towerhe@gmail.com>
2014-08-28 10:04:21 +0800

    Upgrade to ember-cli 0.0.42

--- Brocfile.js
+++ Brocfile.js
@@ -6,8 +6,8 @@ var pickFiles = require('broccoli-static-compiler');
 
 var app = new EmberApp({
   vendorFiles: {
-    'es5-shim.js': 'vendor/es5-shim/es5-shim.js',
-    'es5-sham.js': 'vendor/es5-shim/es5-sham.js'
+    'es5-shim.js': 'bower_components/es5-shim/es5-shim.js',
+    'es5-sham.js': 'bower_components/es5-shim/es5-sham.js'
   }
 });
 
@@ -24,49 +24,49 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/html5shiv/dist/html5shiv.js', { destDir: 'assets' });
-app.import('vendor/respond/dest/respond.min.js', { destDir: 'assets' });
+app.import('bower_components/html5shiv/dist/html5shiv.js', { destDir: 'assets' });
+app.import('bower_components/respond/dest/respond.min.js', { destDir: 'assets' });
 
-app.import('vendor/select2/select2.css');
-app.import('vendor/select2/select2-bootstrap.css');
+app.import('bower_components/select2/select2.css');
+app.import('bower_components/select2/select2-bootstrap.css');
 
-app.import('vendor/jquery.browser/dist/jquery.browser.js');
-app.import('vendor/cldr/plurals.js');
-app.import('vendor/ember-i18n/lib/i18n.js');
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
-app.import('vendor/ember-list-view/index.js');
-app.import('vendor/ember-addons.bs_for_ember/dist/js/bs-core.max.js');
-app.import('vendor/ember-addons.bs_for_ember/dist/js/bs-button.max.js');
-app.import('vendor/ember-addons.bs_for_ember/dist/js/bs-modal.max.js');
-app.import('vendor/select2/select2.js');
-app.import('vendor/select2/select2_locale_zh-CN.js');
-app.import('vendor/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js');
-app.import('vendor/bootstrap-datetimepicker/js/locales/bootstrap-datetimepicker.zh-CN.js');
-app.import('vendor/notifier/notifier.js');
-app.import('vendor/momentjs/moment.js');
-app.import('vendor/momentjs/lang/zh-cn.js');
+app.import('bower_components/jquery.browser/dist/jquery.browser.js');
+app.import('bower_components/cldr/plurals.js');
+app.import('bower_components/ember-i18n/lib/i18n.js');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/ember-list-view/index.js');
+app.import('bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max.js');
+app.import('bower_components/ember-addons.bs_for_ember/dist/js/bs-button.max.js');
+app.import('bower_components/ember-addons.bs_for_ember/dist/js/bs-modal.max.js');
+app.import('bower_components/select2/select2.js');
+app.import('bower_components/select2/select2_locale_zh-CN.js');
+app.import('bower_components/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js');
+app.import('bower_components/bootstrap-datetimepicker/js/locales/bootstrap-datetimepicker.zh-CN.js');
+app.import('bower_components/notifier/notifier.js');
+app.import('bower_components/momentjs/moment.js');
+app.import('bower_components/momentjs/lang/zh-cn.js');
 
-app.import('vendor/gloit-component/dist/globals/gloit-component.js');
+app.import('bower_components/gloit-component/dist/globals/gloit-component.js');
 
-var fontAwesomeAssets = pickFiles('vendor/font-awesome', {
+var fontAwesomeAssets = pickFiles('bower_components/font-awesome', {
   srcDir: 'fonts/',
   files: ['**/*'],
   destDir: '/fonts'
 });
 
-var bootstrapAssets = pickFiles('vendor/bootstrap', {
+var bootstrapAssets = pickFiles('bower_components/bootstrap', {
   srcDir: 'fonts/',
   files: ['**/*'],
   destDir: '/fonts'
 });
 
-var select2Assets = pickFiles('vendor/select2', {
+var select2Assets = pickFiles('bower_components/select2', {
   srcDir: '/',
   files: ['select2.png', 'select2x2.png'],
   destDir: '/assets'
 });
 
-var gloitComponentAssets = pickFiles('vendor/gloit-component', {
+var gloitComponentAssets = pickFiles('bower_components/gloit-component', {
   srcDir: 'images/',
   files: ['**/*'],
   destDir: '/images'
